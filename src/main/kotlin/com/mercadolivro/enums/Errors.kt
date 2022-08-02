c3bb58e2a70df1a08d2bc2ea5dca15ab0b251d7e
Daniel <danielfloresda802@gmail.com>
2022-08-01 22:18:51 -0300

    Added modal Purchase

--- src/main/kotlin/com/mercadolivro/enums/Errors.kt
+++ src/main/kotlin/com/mercadolivro/enums/Errors.kt
@@ -1,6 +1,7 @@
 package com.mercadolivro.enums
 
 enum class Errors(val code: String, val message: String) {
+    ML0001("ML-0001", "Invalid Request"),
     ML1001("ML-1001", "Book [%s] not exists "),
     ML1002("ML-1002", "Cannot updated book with status [%s]"),
     ML2002("ML-1002", "Customer [%s] not exists ")
