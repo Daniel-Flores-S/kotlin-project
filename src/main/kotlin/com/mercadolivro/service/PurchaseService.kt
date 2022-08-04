4b22cc66a843b80873717d5eaa9cf5e896c417e0
Daniel <danielfloresda802@gmail.com>
2022-08-03 22:04:08 -0300

    Created package PurchaseEvent

--- src/main/kotlin/com/mercadolivro/service/PurchaseService.kt
+++ src/main/kotlin/com/mercadolivro/service/PurchaseService.kt
@@ -2,14 +2,17 @@ package com.mercadolivro.service
 
 import com.mercadolivro.model.PurchaseModel
 import com.mercadolivro.repository.PurchaseRepository
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 
 @Service
 class PurchaseService(
     private val purchaseRepository: PurchaseRepository,
+    private val applicationEventPublisher: ApplicationEventPublisher,
 ) {
     fun create(purchaseModel: PurchaseModel) {
-
+        purchaseRepository.save(purchaseModel)
+        applicationEventPublisher.publishEvent()
     }
 
 }
